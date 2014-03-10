453b74dd373d2c03382f4845006941b75eab3ac8
DalekBaldwin <kyle@balding-spiritually.local>
2014-03-09 19:59:09 -0700

    clean up formatting

--- src/shpadoinkle.lisp
+++ src/shpadoinkle.lisp
@@ -26,7 +26,7 @@
 (defmacro setup-readtable (name)
   "Some read-macros I find convenient.
    In Programmer Dvorak, they can all be typed with two adjacent keys. Or one
-   key, in the case of #`."
+   key, in the case of #\# #\`."
   (with-gensyms (stream subchar arg stuff))
   `(eval-when (:compile-toplevel :load-toplevel :execute)
      (unless (find-readtable ,name)
@@ -34,40 +34,48 @@
          (:merge :standard)
          
          ;; #@classname --> (find-class 'classname)
-         (:dispatch-macro-char #\# #\@
-                               (lambda (,stream ,subchar ,arg)
-                                 (declare (ignore ,subchar ,arg))
-                                 `(find-class (quote ,(read ,stream t nil t)))))
+         (:dispatch-macro-char
+          #\# #\@
+          (lambda (,stream ,subchar ,arg)
+            (declare (ignore ,subchar ,arg))
+            `(find-class (quote ,(read ,stream t nil t)))))
          
-         ;; #/(class-name :slot value) -->
-         ;; (make-instance 'class-name :slot value)
-         (:dispatch-macro-char #\# #\/
-                               (lambda (,stream ,subchar ,arg)
-                                 (declare (ignore ,subchar ,arg))
-                                 (let ((,stuff (read ,stream t nil t)))
-                                   `(make-instance ',(car ,stuff) ,@(cdr ,stuff)))))
+         ;; #/(class-name :slot value) --> (make-instance 'class-name :slot value)
+         (:dispatch-macro-char
+          #\# #\/
+          (lambda (,stream ,subchar ,arg)
+            (declare (ignore ,subchar ,arg))
+            (let ((,stuff (read ,stream t nil t)))
+              `(make-instance ',(car ,stuff) ,@(cdr ,stuff)))))
          
-         ;; Ignore entire form. Works just like `#+nil`.
-         ;; Good for commenting out multi-line s-expressions. The reader will
-         ;; still parse the form and complain about things before throwing it
-         ;; away, but I haven't yet bothered to figure out how to stop that.
-         ;; Maybe it's for the best, so you don't leave so much commented-out
-         ;; code hanging around referring to things that haven't existed since
-         ;; twenty commits ago.
-         (:dispatch-macro-char #\# #\!
-                               (lambda (,stream ,subchar ,arg)
-                                 (declare (ignore ,subchar ,arg))
-                                 (read stream t nil t)
-                                 (values)))
+         ;; Ignore entire form. Works just like #+nil.
+         ;; This will essentially comment out a whole s-expression even if it
+         ;; spans multiple lines. The reader will still parse the form and give
+         ;; you warnings about stuff before throwing it away, but I haven't yet
+         ;; bothered to figure out how to stop that.  Maybe it's for the best,
+         ;; because it helps nudge you to delete old commented-out code
+         ;; referring to things that haven't existed since twenty commits
+         ;; ago. Note that if you place two of these in a row, like:
+         ;; `#! #! (form) (form)`
+         ;; they will ignore two forms in a row, but this same behavior happens
+         ;; if you place two #+ tests in a row that fail, so I don't consider it
+         ;; a bug.
+         (:dispatch-macro-char
+          #\# #\!
+          (lambda (,stream ,subchar ,arg)
+            (declare (ignore ,subchar ,arg))
+            (read stream t nil t)
+            (values)))
          
-         ;; (form) --> (copy-tree '(form))
+         ;; #`(form) --> (copy-tree '(form))
          ;; For when it's natural to express a list as a quoted form but you
          ;; can't risk someone else mutating it, since implementations typically
          ;; store quoted forms as constants.
-         (:dispatch-macro-char #\# #\`
-                               (lambda (,stream ,subchar ,arg)
-                                 (declare (ignore ,subchar ,arg))
-                                 `(copy-tree (quote ,(read ,stream t nil t)))))))))
+         (:dispatch-macro-char
+          #\# #\`
+          (lambda (,stream ,subchar ,arg)
+            (declare (ignore ,subchar ,arg))
+            `(copy-tree (quote ,(read ,stream t nil t)))))))))
 
 (defmacro if-not-let (bindings &body (then-form &optional else-form))
   "Like `if-let` for cases when it's more natural to express the else clause first."
