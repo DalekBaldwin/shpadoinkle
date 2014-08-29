b6bfa6e8537590df1338ae59dbaa05d2d066d0ee
DalekBaldwin <DalekBaldwin@users.noreply.github.com>
2014-08-29 03:42:18 -0700

    add dynamic test example

--- test/shpadoinkle-test.lisp
+++ test/shpadoinkle-test.lisp
@@ -34,4 +34,10 @@
     (is (eql a (cddr a))))
   (with-named-labels (b)
       (=# b (list# 1 2 b)) ;; #1=(1 2 #1#)
-    (is (eql b (third b)))))
+    (is (eql b (third b))))
+  (with-named-labels (c d)
+      (=# c (list# 1
+                   2
+                   (if (< 2 3) c d)
+                   (=# d (list# 3 4)))) ;; #1=(1 2 #1# (3 4))
+    (is (eql c (third c)))))
