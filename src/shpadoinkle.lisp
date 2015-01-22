91e47467dd1c4e6a450c91e50a739883e3f2fcc5
DalekBaldwin <DalekBaldwin@users.noreply.github.com>
2015-01-22 05:47:00 -0800

    destructuring-lambda

--- src/shpadoinkle.lisp
+++ src/shpadoinkle.lisp
@@ -243,3 +243,9 @@ format directive on rationals may cause rounding."
   (let* ((scale (expt 10 power)))
     (multiple-value-bind (whole fraction) (floor (/ arg scale))
       (format stream "~D.~V,'0D" whole power (* scale fraction)))))
+
+(defmacro destructuring-lambda (params &rest body)
+  (let ((shallow-params (gensym "SHALLOW-PARAMS")))
+    `(lambda (&rest ,shallow-params)
+       (destructuring-bind (,params) ,shallow-params
+         ,@body))))
